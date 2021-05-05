336c65a7a3ddde4e7df23014cb6275b83114c122
thebialy <67696052+thebialy@users.noreply.github.com>
2021-05-05 10:03:03 -0500

    creat GoogleMap js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import GoogleMap from '.componenets/GoogleMap'
 
 function App() {
   return (
     <div className="App">
-
+        <h1>Google Maps Integration</h1>
+        <GoogleMap />
     </div>
   );
 }
